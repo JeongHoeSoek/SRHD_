1e0f8f4fb17d5cfc2dd87afc185951f017c84092
JeongHoeSoek <whrudtnr5@naver.com>
2024-04-29 15:39:02 +0900

    third

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.cookandroid.myapplication.databinding.FragmentDashboardBinding
@@ -31,9 +32,11 @@ class DashboardFragment : Fragment() {
             val newMessage = binding.messageText.text.toString().trim()
             if (newMessage.isNotEmpty()) {
                 viewModel.saveMessage(newMessage)
+                Toast.makeText(requireContext(), "저장되었습니다.", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
